5e6e611bd9ed7423a6334374bb8e37a4423044f7
Crosscut <crosscutcode@gmail.com>
2022-08-21 17:53:10 -0400

    Project finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-import pageLoad from    './load';
+import {homePageLoad, menuPageLoad, contactPageLoad} from './load';
+    homePageLoad();
+    let content = document.querySelector('#content');
+    
+    let home = document.querySelector('.home');
+    let menu = document.querySelector('.menu');
+    let contact = document.querySelector('.contact');
+   
+    home.addEventListener('click', function(){
+        console.log('home');
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+        homePageLoad();
+        console.log(content);
+        home = document.querySelector('.home');
+        menu = document.querySelector('.menu');
+        contact = document.querySelector('.contact');
+    })
 
-console.log(pageLoad);
+    menu.addEventListener('click', function(){
+        console.log('menu');
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+        menuPageLoad();
+        console.log(content);
+        home = document.querySelector('.home');
+        menu = document.querySelector('.menu');
+        contact = document.querySelector('.contact');
+    })
+
+    contact.addEventListener('click', function(){
+        console.log('contact');
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+        contactPageLoad();
+        console.log(content);
+        home = document.querySelector('.home');
+        menu = document.querySelector('.menu');
+        contact = document.querySelector('.contact');
+    })
+
+   
+
+    
--- src/load.js
+++ src/load.js
@@ -1,5 +1,91 @@
-function pageLoad(){
+    
+function homePageLoad(){
+    let tab = document.createElement('ul');
+    tab.setAttribute('class', 'tab');
 
+    let home = document.createElement('li');
+    let menu = document.createElement('li');
+    let contact = document.createElement('li');
+    home.setAttribute('class', 'home');
+    home.textContent = 'Home';
+    menu.setAttribute('class', 'menu');
+    menu.textContent = 'Menu';
+    contact.setAttribute('class','contact');
+    contact.textContent = 'Contact';
+
+    const content = document.querySelector('#content');
+
+    let h1 = document.createElement('h1');
+    h1.textContent = 'Pizza Place!';
+
+    let img = document.createElement('img');
+    img.setAttribute('src', 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000');
+
+    let p = document.createElement('p');
+    p.textContent = 'Awesome Pizza Parlor!';
+    
+
+    content.appendChild(tab);
+    content.appendChild(h1);
+    content.appendChild(img);
+    content.appendChild(p);
+
+    tab.appendChild(home);
+    tab.appendChild(menu);
+    tab.appendChild(contact);
 }
 
-export default pageLoad;
+function menuPageLoad(){
+    let tab = document.createElement('ul');
+    tab.setAttribute('class', 'tab');
+
+    let home = document.createElement('li');
+    let menu = document.createElement('li');
+    let contact = document.createElement('li');
+    home.setAttribute('class', 'home');
+    home.textContent = 'Home';
+    menu.setAttribute('class', 'menu');
+    menu.textContent = 'Menu';
+    contact.setAttribute('class','contact');
+    contact.textContent = 'Contact';
+
+    const content = document.querySelector('#content');
+
+    let p = document.createElement('p');
+    p.textContent = 'Filler content!';
+    content.appendChild(tab);
+    content.appendChild(p);
+    tab.appendChild(home);
+    tab.appendChild(menu);
+    tab.appendChild(contact);
+    
+    
+}
+
+function contactPageLoad(){
+    let tab = document.createElement('ul');
+    tab.setAttribute('class', 'tab');
+
+    let home = document.createElement('li');
+    let menu = document.createElement('li');
+    let contact = document.createElement('li');
+    home.setAttribute('class', 'home');
+    home.textContent = 'Home';
+    menu.setAttribute('class', 'menu');
+    menu.textContent = 'Menu';
+    contact.setAttribute('class','contact');
+    contact.textContent = 'Contact';
+
+    const content = document.querySelector('#content');
+    
+    let p = document.createElement('p');
+    p.textContent = 'More filler content!';
+    
+    content.appendChild(tab);
+    content.appendChild(p);
+    tab.appendChild(home);
+    tab.appendChild(menu);
+    tab.appendChild(contact);
+}
+
+export {homePageLoad, menuPageLoad, contactPageLoad};
